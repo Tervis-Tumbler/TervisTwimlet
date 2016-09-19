a01ad85dc1383eaf701878fc1a420295d15c61a9
ChrisMagnuson <hourglasssand@gmail.com>
2016-09-19 17:22:21 -0400

    Initial commit

---
+++ TervisTwimlet.psm1
@@ -0,0 +1,17 @@
+﻿#Requires -modules TwiMLPowerShell, TwilioTwimletPowerShell
+
+Function New-TervisTwimletMessageAndRedirectURL {
+    param (
+        $Message,
+        $URL
+    )
+    $TwiMLXMLDocument = New-TwiMLXMLDocument -InnerElements $(
+        New-TwiMLResponse -InnerElements $(
+            $(New-TwiMLSay -Message $Message),
+            $(New-TwiMLRedirect -Method post `
+                -URL $URL
+            )
+        )
+    )
+    New-TwilioTwimletEchoURL -Twiml $TwiMLXMLDocument.OuterXml
+}
